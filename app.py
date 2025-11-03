98641a1a7110819bf91d1e1d647d73b184f5cf6d
Yotan123 <yotanalvintobing@gmail.com>
2025-11-03 03:27:48 +0000

    update17

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import joblib
 
 # Memuat model dan scaler untuk setiap hari
 @st.cache_resource
--- app2.py
+++ app2.py
@@ -16,8 +16,8 @@ def load_model(model_type):
 def load_thresholds():
     return joblib.load('thresholds.pkl')
 
-st.set_page_config(page_title="Prediksi NO₂ Pematangsiantar", page_icon="🌫️")
-st.title("🌫️ Prediksi Kadar NO₂ - Pematangsiantar")
+st.set_page_config(page_title="Prediksi NO₂ Banyuwangi", page_icon="🌫️")
+st.title("🌫️ Prediksi Kadar NO₂ - Banyuwangi")
 st.caption("Prediksi kadar NO₂ troposfer (mol/m²) menggunakan model KNN berdasarkan data Sentinel-5P.")
 
 # Pilihan model prediksi
